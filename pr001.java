0babd00ad55bfff4be33e6c61acd1f0a7f0e4415
mkwlc <kekk2466@list.ru>
2024-11-18 22:28:24 +0700

    Add files via upload

---
+++ pr001.java
@@ -0,0 +1,13 @@
+class pr001 {
+	public static void main(String args[]) {
+		System.out.println("First prog java");
+		//vivodim str na conssole
+		int Var1 = 1024;
+		int Var2;
+		//System.out.println(Var2); ERROR no value
+		Var2 = 411;
+		System.out.println(Var1/2);
+		System.out.println((double)Var2/2);
+		double Var3 = (double)Var2/2;
+	}
+}
